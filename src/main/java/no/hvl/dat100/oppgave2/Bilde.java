173699b7ebd10285c80532f8a81d6191c502c5a9
190331-N <190331@hvl.no>
2025-11-05 12:12:56 +0100

    Update Bilde.java

--- src/main/java/no/hvl/dat100/oppgave2/Bilde.java
+++ src/main/java/no/hvl/dat100/oppgave2/Bilde.java
@@ -8,6 +8,8 @@ public class Bilde extends Tekst {
 	
 	public Bilde(int id, String bruker, String dato, String tekst, String url) {
 		throw new UnsupportedOperationException(TODO.constructor("Bilde"));
+        
+        sdfgjlsdfjgkldmfklgmsdlkgf
 	}
 
 	public Bilde(int id, String bruker, String dato, int likes, String tekst, String url) {
