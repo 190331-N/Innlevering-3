71168c6d943e2e525fcf1913dbc6b6bcee3b8dfc
HansArne04 <hansarnemid@gmail.com>
2025-11-06 11:20:13 +0100

    Update LesBlogg.java

--- src/main/java/no/hvl/dat100/oppgave5/LesBlogg.java
+++ src/main/java/no/hvl/dat100/oppgave5/LesBlogg.java
@@ -19,4 +19,9 @@ public class LesBlogg {
 
 	private static String TEKST = "TEKST";
 	private static String BILDE = "BILDE";
+    public static Blogg les(String mappe, String filnavn) {
+
+        throw new UnsupportedOperationException(TODO.method());
+
+    }
 }
